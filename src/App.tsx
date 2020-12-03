f01ddb54b46e87b867adf17440e2e024b24943b0
Albert Dugba <albert.dugba@gmail.com>
2020-12-03 14:15:11 +0000

    react typescript setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { AppContainer } from "./styles/styles";
+import Column from "./components/Column";
+import Card from "./components/Card";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="App Scafold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Adding Google maps to app" />
+      </Column>
+      <Column text="To Do">
+        <Card text="App Scafold" />
+      </Column>
+      <Column text="Done">
+        <Card text="Write unit testing" />
+      </Column>
+    </AppContainer>
   );
-}
+};
 
 export default App;
